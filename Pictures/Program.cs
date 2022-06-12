70303b66402cb4abb6381efaad6dd4b5bb0d6f18
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 21:26:07 +0300

    Добавьте файлы проекта.

---
+++ Pictures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int totalPictures = 52;
+            int extraPictures = totalPictures % picturesInRow;
+            int filledRows = (totalPictures - extraPictures) / picturesInRow; 
+
+            Console.WriteLine($"Полностью заполненных рядов с картинками : {filledRows} , картинок сверх меры : {extraPictures}");
+        }
+    }
+}
